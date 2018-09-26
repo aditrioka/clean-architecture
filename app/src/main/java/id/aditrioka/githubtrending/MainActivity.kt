2816040167454c3440df3b06e9701e6193e3241d
Adi Trioka <aditrioka@gmail.com>
2018-09-26 14:45:49 +0700

    Initial commit

---
+++ app/src/main/java/id/aditrioka/githubtrending/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.aditrioka.githubtrending
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
