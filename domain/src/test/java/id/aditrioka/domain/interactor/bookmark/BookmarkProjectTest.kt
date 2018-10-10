f5ac7b2df94c50b0b393270ecf6f8178fa14762a
Adi Trioka <aditrioka@gmail.com>
2018-10-10 23:26:47 +0700

    Create test for bookmark project

---
+++ domain/src/test/java/id/aditrioka/domain/interactor/bookmark/BookmarkProjectTest.kt
@@ -0,0 +1,44 @@
+package id.aditrioka.domain.interactor.bookmark
+
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.whenever
+import id.aditrioka.domain.executor.PostExecutionThread
+import id.aditrioka.domain.repository.ProjectsRepository
+import id.aditrioka.domain.test.ProjectDataFactory
+import io.reactivex.Completable
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import java.lang.IllegalArgumentException
+
+class BookmarkProjectTest {
+
+    private lateinit var bookmarkProject: BookmarkProject
+    @Mock lateinit var projectsRepository: ProjectsRepository
+    @Mock lateinit var postExecutionThread: PostExecutionThread
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        bookmarkProject = BookmarkProject(projectsRepository, postExecutionThread)
+    }
+
+    @Test
+    fun bookmarkProjectCompletes() {
+        stubBookmarkProject(Completable.complete())
+        val testObserver = bookmarkProject.buildUseCaseCompletable(
+                BookmarkProject.Params.forProject(ProjectDataFactory.randomUuid())).test()
+        testObserver.assertComplete()
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun bookmarkProjectThrowsException() {
+        bookmarkProject.buildUseCaseCompletable().test()
+    }
+
+    private fun stubBookmarkProject(completable: Completable) {
+        whenever(projectsRepository.bookmarkProject(any()))
+                .thenReturn(completable)
+    }
+}
